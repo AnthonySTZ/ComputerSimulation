2a2a26df93e8565a83ef95b41957ef3c53097e32
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-09 18:10:49 +0200

    add test or_gate

--- computer_simulation/src/main.py
+++ computer_simulation/src/main.py
@@ -2,6 +2,7 @@ from game_renderer import Renderer
 from input import Input
 from output import Output
 from gates import AndGate, OrGate
+import pygame as pg
 
 if __name__ == "__main__":
 
@@ -11,6 +12,9 @@ if __name__ == "__main__":
     input_2 = Input((200, 300))
     and_gate = AndGate((300, 250))
     output = Output((400, 250))
+
+    or_gate = OrGate((300, 350))
+
     input.connect_to(and_gate, 0, 0)  # Connect the input item to the AND gate
     input_2.connect_to(and_gate, 0, 1)  # Connect the input item to the AND gate
     and_gate.connect_to(output, 0, 0)  # Connect the AND gate output to the output item
@@ -18,5 +22,6 @@ if __name__ == "__main__":
     game.add_item(input_2)  # Add an input item to the game
     game.add_item(and_gate)  # Add an AND gate to the game
     game.add_item(output)  # Add an input item to the game
+    game.add_item(or_gate)  # Add an OR gate to the game
 
     game.run()
